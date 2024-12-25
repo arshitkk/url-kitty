efc618d1865dcca872befb804ab622a472dca38e
Arshit Kumar <arshitkumar222@gmail.com>
2024-12-25 22:40:52 +0530

    fixed the vite config and created vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: "./index.html",
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"], // Split vendor libraries into a separate chunk
+        },
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true, // Enable for client-side routing
+  },
+});
